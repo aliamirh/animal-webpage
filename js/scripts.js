e3d83a482ab0575c01047d3fe5539464fe30b5c5
Ali Amirhamzeh and Jake Jensen <ali@>
2019-08-15 13:48:54 -0700

    create the drop down menu into a form

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document) .ready(function()){
+  function  onclick
+}
+
+// this creates our first global  function and it is ready from the beggining of opening the page becasuse of the ready before function
+$(document).ready(function() {
+askUser();
+});
+// this function will repeat if the user get to the end of the else function
+function askUser() {
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#ballet').show();
+  } else if (age < 18) {
+     $("#under-18").show();
+  } else {
+     alert("numbers only please")
+     askUser();
+  }
+}
